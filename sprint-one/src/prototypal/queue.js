2166b1f8d414221cf0f9db480854fe1cce59f097
Jeff Chasin <jeff@jeffchasin.com>
2018-07-04 22:03:40 -0400

    fix prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // FIFO
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.firstIn = 1;
+  newQueue.lastIn = 1;
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) {
+    this.storage[this.lastIn] = value;
+    this.lastIn++;
+  },
+
+  dequeue: function() {
+    var dequeued = this.storage[this.firstIn];
+    delete this.storage[this.firstIn];
+    this.firstIn++;
+    return dequeued;
+  },
+
+  size: function() {
+    return this.lastIn - this.firstIn >= 0 ? this.lastIn - this.firstIn : 0;
+  }
+};
 
 
