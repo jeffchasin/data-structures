a6c531af4fe764245fbafe478108740fa0c34aa0
Jeff Chasin <jeff@jeffchasin.com>
2018-07-05 15:39:28 -0400

    fix pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // FIFO
+  this.storage = {};
+  this.firstIn = 1;
+  this.lastIn = 1;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.lastIn] = value;
+  this.lastIn++;
+};
 
+Queue.prototype.dequeue = function() {
+  var dequeued = this.storage[this.firstIn];
+  delete this.storage[this.firstIn];
+  this.firstIn++;
+  return dequeued;
+};
+
+Queue.prototype.size = function() {
+  return this.lastIn - this.firstIn >= 0 ? this.lastIn - this.firstIn : 0;
+};
+
+var sandwichLine = new Queue();
