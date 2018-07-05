d2a84a6666b6b28d51cdfee75de68f988be29654
Jeff Chasin <jeff@jeffchasin.com>
2018-07-05 15:22:10 -0400

    fix pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,6 +2,21 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // LIFO
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
 
+Stack.prototype.pop = function() {
+  var popped = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length >= 0 ? Object.keys(this.storage).length : 0;
+};
+
+var historyObj = new Stack();
