89db12fcb33cc254ae061c680a1d811ae04f6c9b
Jeff Chasin <jeff@jeffchasin.com>
2018-07-05 21:24:01 -0400

    fix es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // LIFO
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  }
+
+  pop() {
+    var popped = this.storage[Object.keys(this.storage).length - 1];
+    delete this.storage[Object.keys(this.storage).length - 1];
+    return popped;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var history = new Stack();
